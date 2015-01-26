5148acbbc433ce25774f81932d9ffea44a699152
pGrimaud <pascalgrimaud@gmail.com>
2015-01-26 22:06:34 +0100

    Simple page with AngularJS example

---
+++ angularjs/ServerHttp.java
@@ -0,0 +1,39 @@
+
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.platform.Verticle;
+
+import java.util.Map;
+
+public class ServerHttp extends Verticle {
+
+  public void start() {
+    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+      public void handle(HttpServerRequest req) {
+
+	if (req.uri().equals("/")) {
+	  req.response().sendFile("index.html");
+	} else {
+          req.response().setStatusCode(404);
+          req.response().end();
+        }
+      }
+    }).listen(8080);
+  }
+}
